15b7a6e777ae0537311d774944ead18125005a6e
etemkeskin <etemkeskin@yahoo.com>
2025-02-15 21:27:22 +0300

    first commit

---
+++ frontend/src/app/(authorized)/users/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default async function Users() {
+  return (
+    <div className="container max-w-xl">
+      <div className="grid grid-flow-row gap-2 p-4">
+        İLK SAYFA
+      </div>
+    </div>
+  );
+}
